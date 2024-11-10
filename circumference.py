5634585c10958b43f2910fdbac9d26983e518dbe
LeoCroak <angadhaliwal2@gmail.com>
2024-11-10 14:40:03 +0000

    Circumference Calculator

--- circumference.py
+++ circumference.py
@@ -1,9 +1,16 @@
-# original code found on https://github.com/LeoCroak/Random-things/blob/main/circumference.py
+#original code found on https://github.com/LeoCroak/Random-things/blob/main/circumference.py
 import math
 
-diameter = float(input("Enter your diameter:"))
-radius = diameter / 2
+diameter = input("Enter your diameter:")
 unit = input("Enter your unit:")
+
+while True:
+    try:
+        diameter = float(diameter)
+        radius = diameter / 2
+        break
+    except ValueError:
+        diameter = input("Enter a valid diameter:")
 
 circumference = 2 * radius * math.pi
 
