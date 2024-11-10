6bf9cc82be9775f8dbceab619c14518ef59e37eb
LeoCroak <angadhaliwal2@gmail.com>
2024-11-10 14:28:21 +0000

    Countdown in C#

--- countdown.cs
+++ countdown.cs
@@ -1,20 +1,19 @@
 // original code found on https://github.com/LeoCroak/Random-things/blob/main/countdown.cs
 using System;
-using System.Threading;
+using System.Threading; // Corrected the namespace
 
 namespace Countdown {
     class Timerlogic {
         public static void Main(string[] args) {
 
-            int chosentime = int.Parse(Console.ReadLine()); // collects user input 
+            int chosentime = int.Parse(Console.ReadLine()); // Collects user input 
 
-            for (chosentime; chosentime > 0; chosentime--) // timer logic
-            {
-                Console.WriteLine(chosentime)
-                Thread.Sleep(1000)
+            for (chosentime; chosentime > 0; chosentime--) { // Timer logic
+                Console.WriteLine(chosentime); // Display the countdown
+                Thread.Sleep(1000); // Wait for 1 second (1000 milliseconds)
             }
 
-            Console.WriteLine("Time is up")
+            Console.WriteLine("Time is up!"); // Message when the countdown reaches 0
         }
     }
 }
